bb5a6bc55db8b65e60ce58ae7b01a43efc7369f9
Reena <Kreenasree@gmail.com>
2025-04-23 22:34:49 +0530

    deploy#2

--- server/index.js
+++ server/index.js
@@ -29,8 +29,8 @@ app.get("/ping", (_req, res) => {
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-const server = app.listen(process.env.PORT, () =>
-  console.log(`Server started on ${process.env.PORT}`)
+const server = app.listen(5000, () =>
+  console.log(`Server started on 5000`)
 );
 const io = socket(server, {
   cors: {
